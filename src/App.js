d27ea4614d913a05b8fcf44eae7f198fe31619cf
Jai Ho <choeco2020@gmail.com>
2022-07-25 23:30:57 +0900

    added banner and room infos

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
+import './App.css';
+import arthall from './img/arthall.jpg'
 
 function App() {
   return (
@@ -16,11 +18,43 @@ function App() {
             <Nav className="me-auto">
               <Nav.Link href="#home">소개</Nav.Link>
               <Nav.Link href="#link">예약</Nav.Link>
+              <Nav.Link href="#link">오시는길</Nav.Link>
 
             </Nav>
           </Navbar.Collapse>
         </Container>
       </Navbar>
+
+      <div className='main-bg'></div>
+
+      <div className="container">
+  <div className="row">
+    <div className="col-md-4">
+      <img src={arthall} width="80%"/>
+      <h4>Art Hall</h4>
+      <button>예약</button>
+      
+      </div>
+
+      <div className="col-md-4">
+      <img src={arthall} width="80%"/>
+      <h4>Art Hall</h4>
+      <button>예약</button>
+      
+      </div>
+
+      <div className="col-md-4">
+      <img src={arthall} width="80%"/>
+      <h4>Art Hall</h4>
+      <button>예약</button>
+      
+      </div>
+
+      
+    
+  </div>
+</div> 
+      
     </div>
   );
 }
